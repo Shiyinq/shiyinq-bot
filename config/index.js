6191cebc0a3311724a031c441706cb9d0f25e6fe
Shiyinq <agunpermana25@gmail.com>
2018-10-22 12:24:58 +0700

    merapihkan code dengan eslint standard

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 module.exports = {
-    token_bot : process.env.TOKEN_BOT,
-    token_newsapi : process.env.TOKEN_NEWSAPI
+  token_bot: process.env.TOKEN_BOT,
+  token_newsapi: process.env.TOKEN_NEWSAPI
 }
--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = require('./config/index').token_bot;
-const brain = require('./app/brain/index');
+const TelegramBot = require('node-telegram-bot-api')
+const token = require('./config/index').token_bot
+const brain = require('./app/brain/index')
 
-const bot = new TelegramBot(token, {polling: true});
- 
+const bot = new TelegramBot(token, { polling: true })
 // Matches "/echo [whatever]"
 // /\/echo (.+)/
 bot.onText(/\/echo (.+)/, (msg, match) => {
-
-  const chatId = msg.chat.id;
-  const resp = match[1];
- 
-  bot.sendMessage(chatId, resp);
-});
- 
-
+  const chatId = msg.chat.id
+  const resp = match[1]
+  bot.sendMessage(chatId, resp)
+})
 bot.on('message', (msg) => {
-
   switch (msg.text.toLowerCase()) {
     case '/start':
-      brain.start(msg, bot);
-      break;
+      brain.start(msg, bot)
+      break
     case '/help':
-      brain.start(msg, bot);
-      break;
+      brain.start(msg, bot)
+      break
     case '/menu':
-      brain.menuBot(msg, bot);
-      break;
+      brain.menuBot(msg, bot)
+      break
     case '/qrcode':
-      brain.qrcodedesc(msg, bot);
-      break;
+      brain.qrcodedesc(msg, bot)
+      break
     case '/lorem':
-      brain.lorem(msg, bot);
-      break;
+      brain.lorem(msg, bot)
+      break
     case '/beritateknologi':
-      brain.berita(msg, bot);
-      break;
+      brain.berita(msg, bot)
+      break
     case '/quoterandom':
-      brain.quoteRandom(msg, bot);
-      break;
+      brain.quoteRandom(msg, bot)
+      break
     case 'ping':
-      brain.ping(msg, bot);
-      break;
+      brain.ping(msg, bot)
+      break
     default:
-      console.log('');
-      // brain.responDefault(msg, bot);
-
+      console.log('')
+      // brain.responDefault(msg, bot)
   }
- 
-});
+})
