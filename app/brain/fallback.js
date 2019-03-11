2e9a688f5386ac6b3d3f2b6882ce17991287dadf
Shiyinq <agunpermana25@gmail.com>
2019-03-11 09:11:00 +0700

    merapihkan syntax

--- app/brain/fallback.js
+++ app/brain/fallback.js
@@ -1,14 +1,17 @@
 const tools = require('../tools')
 
 let resBot = null
-module.exports = {
-  responDefault: function (msg, bot) {
+class Fallback {
+  responDefault (msg, bot) {
     resBot = ['😥 Maaf saya tidak mengerti ka..', 'Maksudnya apa ka ? 🤔', 'Aku gak mudeng ka 🙄', 'Hmmm 🤔', 'Bingung saya 😵', 'Gak ngerti aku..😓', 'Ngomong apa sih ka ? 🙄']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
-  },
-  errorMessage: function (msg, bot) {
+  }
+  errorMessage (msg, bot) {
     resBot = 'Maaf terjadi kesahalan'
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
   }
 }
+
+const fallback = new Fallback()
+module.exports = fallback
--- app/brain/smalltalk.js
+++ app/brain/smalltalk.js
@@ -2,35 +2,38 @@ const tools = require('../tools')
 
 let resBot = null
 
-module.exports = {
-  namaBot: function (msg, bot) {
+class Smalltalk {
+  namaBot (msg, bot) {
     resBot = ['Nama saya Bot', 'Saya Bot']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
-  },
-  sapaBot: function (msg, bot) {
+  }
+  sapaBot (msg, bot) {
     resBot = ['Hi', 'helo', 'hay', 'Halo']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
-  },
-  selamatPagi: function (msg, bot) {
+  }
+  selamatPagi (msg, bot) {
     resBot = ['Selamat pagi juga', 'Pagi gan', 'Pagee']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
-  },
-  selamatSiang: function (msg, bot) {
+  }
+  selamatSiang (msg, bot) {
     resBot = ['Selamat siang juga', 'Siang', 'Siyang']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
-  },
-  selamatSore: function (msg, bot) {
+  }
+  selamatSore (msg, bot) {
     resBot = ['Selamat sore juga', 'sore', 'soree']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
-  },
-  selamatMalam: function (msg, bot) {
+  }
+  selamatMalam (msg, bot) {
     resBot = ['Selamat malam juga', 'malam', 'malem']
     resBot = tools.responseVarian(resBot)
     bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
   }
 }
+
+const smalltalk = new Smalltalk()
+module.exports = smalltalk
