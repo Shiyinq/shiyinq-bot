7854479f9da11990ea0934d33f260196c29b7b92
Shiyinq <agunpermana25@gmail.com>
2019-03-11 10:09:49 +0700

    memindahkan let resBot ke constructor

--- app/brain/smalltalk.js
+++ app/brain/smalltalk.js
@@ -1,37 +1,38 @@
 const tools = require('../tools')
 
-let resBot = null
-
 class Smalltalk {
+  constructor () {
+    this.resBot = null
+  }
   namaBot (msg, bot) {
-    resBot = ['Nama saya Bot', 'Saya Bot']
-    resBot = tools.responseVarian(resBot)
-    bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
+    this.resBot = ['Nama saya Bot', 'Saya Bot']
+    this.resBot = tools.responseVarian(this.resBot)
+    bot.sendMessage(msg.chat.id, this.resBot, { reply_to_message_id: msg.message_id })
   }
   sapaBot (msg, bot) {
-    resBot = ['Hi', 'helo', 'hay', 'Halo']
-    resBot = tools.responseVarian(resBot)
-    bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
+    this.resBot = ['Hi', 'helo', 'hay', 'Halo']
+    this.resBot = tools.responseVarian(this.resBot)
+    bot.sendMessage(msg.chat.id, this.resBot, { reply_to_message_id: msg.message_id })
   }
   selamatPagi (msg, bot) {
-    resBot = ['Selamat pagi juga', 'Pagi gan', 'Pagee']
-    resBot = tools.responseVarian(resBot)
-    bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
+    this.resBot = ['Selamat pagi juga', 'Pagi gan', 'Pagee']
+    this.resBot = tools.responseVarian(this.resBot)
+    bot.sendMessage(msg.chat.id, this.resBot, { reply_to_message_id: msg.message_id })
   }
   selamatSiang (msg, bot) {
-    resBot = ['Selamat siang juga', 'Siang', 'Siyang']
-    resBot = tools.responseVarian(resBot)
-    bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
+    this.resBot = ['Selamat siang juga', 'Siang', 'Siyang']
+    this.resBot = tools.responseVarian(this.resBot)
+    bot.sendMessage(msg.chat.id, this.resBot, { reply_to_message_id: msg.message_id })
   }
   selamatSore (msg, bot) {
-    resBot = ['Selamat sore juga', 'sore', 'soree']
-    resBot = tools.responseVarian(resBot)
-    bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
+    this.resBot = ['Selamat sore juga', 'sore', 'soree']
+    this.resBot = tools.responseVarian(this.resBot)
+    bot.sendMessage(msg.chat.id, this.resBot, { reply_to_message_id: msg.message_id })
   }
   selamatMalam (msg, bot) {
-    resBot = ['Selamat malam juga', 'malam', 'malem']
-    resBot = tools.responseVarian(resBot)
-    bot.sendMessage(msg.chat.id, resBot, { reply_to_message_id: msg.message_id })
+    this.resBot = ['Selamat malam juga', 'malam', 'malem']
+    this.resBot = tools.responseVarian(this.resBot)
+    bot.sendMessage(msg.chat.id, this.resBot, { reply_to_message_id: msg.message_id })
   }
 }
 
