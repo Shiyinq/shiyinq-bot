0c14e488a0cacf4da707ccaeb952a3815bbe0be7
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2020-03-21 12:17:58 +0700

    update

--- bot.js
+++ bot.js
@@ -3,9 +3,16 @@ require('dotenv').config()
 const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+const express = require('express')
+const app = express()
+
 require('fs').readdirSync('./commands')
   .forEach(function (file) {
     require('./commands/' + file)(bot)
   })
 
 bot.launch()
+
+app.get('/', (req, res) => res.send("Hello world!, I'm Bot\n"))
+
+app.listen(3000)
---
+++ commands/randomPhoto.js
@@ -0,0 +1,17 @@
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+module.exports = (bot) => {
+  bot.command('randomphoto', (ctx) => {
+    let category = ctx.message.text.split(' ')[1]
+    let photoURL = null
+    photoURL = `https://source.unsplash.com/1600x900/?${category}`
+
+    const keyboard = Markup.inlineKeyboard([
+      Markup.urlButton('Srouce', 'https://source.unsplash.com')
+    ])
+
+    ctx.reply('Tunggu sebentar yaa, lagi nyari gambarnya..')
+    ctx.replyWithPhoto({ url: photoURL }, Extra.markup(keyboard))
+  })
+}
