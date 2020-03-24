3fc7369cdd67aaab87defe89a4a74025c06083f6
Shiyinq <shiyinq.github.io@gmail.com>
2020-03-24 14:54:27 +0700

    add command date & menu

---
+++ commands/menu.js
@@ -0,0 +1,13 @@
+const commands = require('../commands.json')
+
+module.exports = (bot) => {
+  bot.command('menu', (ctx) => {
+    let listCommand = '💻 List Command\n\n'
+
+    commands.forEach(cmd => {
+      listCommand += `/${cmd.name} ${cmd.parameter} - ${cmd.description}\n`
+    })
+
+    ctx.reply(listCommand)
+  })
+}
