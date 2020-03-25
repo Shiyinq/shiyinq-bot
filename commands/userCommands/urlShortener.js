2c0de50bf580312b2f0cb793357f6ecde4a9a2ff
Shiyinq <shiyinq.github.io@gmail.com>
2020-03-25 13:55:02 +0700

    add command urlshort

---
+++ commands/userCommands/urlShortener.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+
+module.exports = (bot) => {
+  bot.command('urlshort', (ctx) => {
+    let param = ctx.message.text.split(' ')[1]
+    let baseUrl = 'https://rel.ink/'
+    if (param) {
+      let expression = /[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)?/gi
+      let regex = new RegExp(expression)
+      let t = param
+
+      if (t.match(regex)) {
+        axios.post(`${baseUrl}api/links/`, { url: param })
+          .then(({ data: { hashid } }) => {
+            ctx.reply(`${baseUrl}${hashid}`)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } else {
+        ctx.reply('Bukan url yang dimasukan')
+      }
+    } else {
+      ctx.reply('Masukan url yang akan di perpendek')
+    }
+  })
+}
