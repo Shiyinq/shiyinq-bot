2aac9d2730351282003959046976a68c28b54e44
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2020-03-25 21:07:39 +0700

    add response time in debug & ping command

--- commands/userCommands/debug.js
+++ commands/userCommands/debug.js
@@ -1,6 +1,8 @@
 module.exports = (bot) => {
   bot.command('/dbg', (ctx) => {
+    const start = new Date()
+    const ms = new Date() - start
     let dbg = JSON.stringify(ctx.update, undefined, 2)
-    ctx.replyWithHTML(`<b>Debug messages</b>\n<code>${dbg}</code>`)
+    ctx.replyWithHTML(`<b>Debug messages</b>\n<code>${dbg}\n\n⏱ ${ms} s</code>`)
   })
 }
