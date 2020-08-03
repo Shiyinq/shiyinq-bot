45cec183d62e2ebad4be09d6600a112a69fb6ae7
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2020-08-03 10:42:30 +0700

    update geoip

--- commands/userCommands/geoIP.js
+++ commands/userCommands/geoIP.js
@@ -5,20 +5,16 @@ module.exports = (bot) => {
   bot.command(command, (ctx) => {
     let param = ctx.message.text.split(' ')
     let ip = param[1]
-    let baseURL = 'http://ip-api.com/json'
+    let baseURL = 'http://ip-api.com'
 
-    if (!ip) {
-      ctx.reply('Silahkan masukan ip setelah command\n\nContoh: /gip 64.233.160.0')
-    } else {
-      axios.get(`${baseURL}/${ip}`)
-        .then(({ data }) => {
-          let obj = JSON.stringify(data, undefined, 2)
-          ctx.replyWithHTML(`<b>IP: ${ip}\n\n </b><code>${obj}</code>\n\nSumber: ip-api.com`)
-        })
-        .catch(error => {
-          console.log(error)
-          ctx.reply('Sepertinya ada gangguan, coba lagi lain kali')
-        })
-    }
+    axios.get(`${baseURL}${ip ? '/json/' + ip : '/json'}`)
+      .then(({ data }) => {
+        let obj = JSON.stringify(data, undefined, 2)
+        ctx.replyWithHTML(`<b>${ip ? 'IP: ' + ip : 'Your IP: ' + data.query}\n\n </b><code>${obj}</code>\n\nSumber: ip-api.com`)
+      })
+      .catch(error => {
+        console.log(error)
+        ctx.reply('Sepertinya ada gangguan, coba lagi lain kali')
+      })
   })
 }
